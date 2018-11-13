9302d43d4ac1cdebe3fd654a131b549a1422a31b
mmoneta <jjaymatt@gmail.com>
2018-11-13 12:46:41 +0100

    init

--- Models/URL_Shortener.php
+++ Models/URL_Shortener.php
@@ -3,9 +3,13 @@
     public function select_link_by_key($key) {
       $sql = "SELECT `link` FROM `urls` WHERE `key`=?";
       $req = Database::getBdd()->prepare($sql);
-      $req-> execute([$key]); 
-      $link = $req-> fetchObject();
-      return $link-> link;
+      $req-> execute([$key]);
+      $count = $req->rowCount();
+      if ($count > 0) {
+        $link = $req-> fetchObject();
+        return $link-> link;
+      }
+      return false;
     }
 
     public function insert($data) {
